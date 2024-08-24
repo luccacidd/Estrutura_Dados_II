afcfe81bf0f7d93f3aa1c820607394e3edcc6a45
luccacidd <161381857+luccacidd@users.noreply.github.com>
2024-08-23 21:48:55 -0300

    Função createNode
    Implementação de uma função para criar o nó;

--- Meu/MinhaArvre.c
+++ Meu/MinhaArvre.c
@@ -7,3 +7,10 @@ typedef struct {
   struct Node *left;
   struct Node *right;
 } Node;
+
+Node *createNode(int data){
+  Node* newNode = (Node*)malloc(sizeof(Node));
+  newNode->data = data;
+  newNode->left = NULL;
+  newNode->right = NULL;
+}
